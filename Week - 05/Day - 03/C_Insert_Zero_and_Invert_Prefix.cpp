7c0d5e8d0d090eadf74efc2bc651c499bb35fe3e
Tamim0133 <tamimdewanandnobody@gmail.com>
2023-07-09 23:44:03 +0600

    Day 3

---
+++ Week - 05/Day - 03/C_Insert_Zero_and_Invert_Prefix.cpp
@@ -0,0 +1,163 @@
+// #include <bits/stdc++.h>
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+// #include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+using ll = long long int;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    if (arr[n - 1] == 1)
+    {
+        cout << "NO" << endl;
+        return;
+    }
+
+    cout << "YES" << endl;
+
+    vector<int> ans;
+
+    for (int i = n - 1; i > -1; i--)
+    {
+        if (arr[i] == 0)
+        {
+            if (i == 0 or arr[i - 1] == 0)
+            {
+                ans.push_back(0);
+            }
+            else if (arr[i - 1] == 1)
+            {
+                int cnt = 0, temp;
+                int j = i - 1;
+                int nxt;
+
+                while (j > -1 and arr[j] == 1)
+                {
+                    cnt++;
+                    nxt = j;
+                    j--;
+                }
+
+                temp = cnt;
+
+                while (cnt--)
+                {
+                    ans.push_back(0);
+                }
+                ans.push_back(temp);
+                i = nxt + 1;
+            }
+        }
+    }
+
+    for (auto i : ans)
+        cout << i << " ";
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
