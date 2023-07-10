85003aa074063059d0eae59c32019445414227ae
Tamim0133 <tamimdewanandnobody@gmail.com>
2023-07-10 23:54:38 +0600

    Dat 5

---
+++ Week - 05/Day - 03/C_Copil_Copac_Draws_Trees.cpp
@@ -0,0 +1,148 @@
+// #include <bits/stdc++.h>
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+// #include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+using ll = long long int;
+
+const int N = 200005;
+vector<int> adj_list[N];
+int visited[N];
+
+void dfs(int node)
+{
+
+    cout << node << " ";
+    visited[node] = 1;
+
+    for (auto adj_node : adj_list[node])
+    {
+        if (visited[adj_node] == 0)
+        {
+            dfs(adj_node);
+        }
+    }
+    cout << endl;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        adj_list[i].clear();
+        visited[i] = 0;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
+
+    // cout << dfs(1) << endl;
+    dfs(1);
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
