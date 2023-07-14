20c7ced66878ea8564ebc178ca5afc6b91a6c9ac
Tamim0133 <tamimdewanandnobody@gmail.com>
2023-07-14 23:58:18 +0600

    OK

---
+++ Week - 06/Day - 01/Rectangular_Tiling.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if (a & 1 and b & 1)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+    return 0;
+}
