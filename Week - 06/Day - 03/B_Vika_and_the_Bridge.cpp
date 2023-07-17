9ae4b317cf6238036014486286e5e50132af012d
Tamim0133 <tamimdewanandnobody@gmail.com>
2023-07-17 12:52:30 +0600

    Day -4

---
+++ Week - 06/Day - 03/B_Vika_and_the_Bridge.cpp
@@ -0,0 +1,150 @@
+// #include <bits/stdc++.h>
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+// #include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+using ll = long long int;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int ans = 0, temp = 0;
+
+    bool ok = false;
+    for (int j = 1; j <= n; j++)
+    {
+        bool found = false;
+        int turn = 0;
+        // for (int i = 0 + ans;; i += (ans + 1))
+        int i = 0 + ans;
+        while (1)
+        {
+            cout << i << " ";
+            if (i >= n)
+            {
+                ok = true;
+                break;
+            }
+            else if (turn != 0 and turn != 1 and arr[i] != arr[i - ans - 1])
+            {
+                bool found = true;
+                break;
+            }
+            turn++;
+            i += (ans + 1);
+        }
+        ans++;
+        if (ok)
+        {
+            break;
+        }
+    }
+    cout << endl;
+    cout << "ANS" << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
