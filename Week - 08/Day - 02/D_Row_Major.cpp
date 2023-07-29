1ae0b2b39478fba22ad3d4e7d2436e6d2d846212
Tamim0133 <tamimdewanandnobody@gmail.com>
2023-07-29 17:18:10 +0600

    Day 2 done

---
+++ Week - 08/Day - 02/D_Row_Major.cpp
@@ -0,0 +1,156 @@
+// #include <bits/stdc++.h>
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+// #include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+using ll = long long int;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    string s = "";
+    vector<char> base(27);
+    if (n == 1)
+    {
+        cout << 'a' << endl;
+        return;
+    }
+
+    else if (n == 2)
+    {
+        cout << "ab" << endl;
+        return;
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i != 0)
+        {
+            ans = i;
+            break;
+        }
+    }
+
+    for (int i = 1; i <= 26; i++)
+    {
+        base[i] = 'a' + i - 1;
+    }
+
+    int rem = n % ans;
+    int div = n / ans;
+    for (int i = 1; i <= ans; i++)
+    {
+        s += base[i];
+    }
+
+    for (int i = 1; i <= div; i++)
+    {
+        cout << s;
+    }
+    for (int i = 0; i < rem; i++)
+    {
+        cout << s[i];
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
